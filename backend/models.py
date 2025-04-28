78f9c242b70df540792666ec63d511a26883074c
Tanish Rajput <tanishrajput9@gmail.com>
2025-04-28 12:42:38 +0530

    added models.py

---
+++ backend/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+class Report(db.Model):
+    __tablename__ = 'reports'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    image_filename = db.Column(db.String(255), nullable=False)
+    prediction_result = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('reports', lazy=True))
+
+
+
