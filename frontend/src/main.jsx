d0560c932099816ee38bab01b8120f11d417a541
Tanish Rajput <tanishrajput9@gmail.com>
2025-09-26 23:36:11 +0530

    updated main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const clerkPubKey = "pk_test_Y2xhc3NpYy1zaHJpbXAtOTUuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ClerkProvider publishableKey={clerkPubKey}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
+);
+
