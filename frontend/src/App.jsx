7206b58a1152b306d6c8730beb3b881180a3044b
Tanish Rajput <tanishrajput9@gmail.com>
2025-09-26 23:35:28 +0530

    updated app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,19 @@
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Signup } from "../src/components/SignUp/Signup";
 import { Signin } from "../src/components/SignIn/Signin";
 import { Dashboard } from "../src/components/Dashboard/Dashboard";
+import  HistoryPage  from "./components/History/HistoryPage";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/history" element={<HistoryPage />} />
+    </Routes>
   );
 }
 
